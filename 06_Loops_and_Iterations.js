f4d079cc80ec379e8731d82e7c7e29484868695f
hectic4491 <finalrob@gmail.com>
2025-01-13 20:55:02 -0500

    Working on chapter 6

---
+++ 06_Loops_and_Iterations.js
@@ -0,0 +1,18 @@
+// Loops and Iterations
+
+/** The for Loop
+ * The for loop is a standard control-flow construct in many programming
+ * languages, including JavaScript. It's commonly used to iterate over 
+ * given sequences or iterate a known number of times and execute a piece
+ * of code for each iteration.
+ */
+
+for (let i = 0; i < 5; i++) {
+  console.log(`Step: ${i}`);
+}
+
+myString = "Elephant"
+for (let i = 0; i <myString.length; i++) {
+  console.log(myString[i])
+}
+
