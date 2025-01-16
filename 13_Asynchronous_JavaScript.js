8343a59252c8b6b9fb2858d2b8b6143c02b73ff2
hectic4491 <finalrob@gmail.com>
2025-01-16 15:58:34 -0500

    Working on chapter 13

---
+++ 13_Asynchronous_JavaScript.js
@@ -0,0 +1,20 @@
+// Asynchronous JavaScript
+
+/**Asynchronous programmins is a technique that enables your program to
+ * start a potentially long-running task and still be able to be
+ * responsive to other events that task runs, rather then having to wait
+ * until that task has finished. Once that task has finished, your
+ * program is presented with the result.
+ * 
+ * Many functions provided by browsers, especially the most interesting
+ * ones, can potentially take a long time, and therefore, are
+ * asynchronous. For example:
+ * 
+ * > Making HTTP requests using 'fetch()'
+ * > Accessing a user's camera of microphone using 'getUserMedia()'
+ * > Asking a user to select files using 'showOpenFilePicker()'
+ * 
+ * So even though you may not have to implement your own asynchronous
+ * functions very often, you are very likely to need to use them
+ * correctly.
+ */
