72a932bc3ffd897fe8534b1f354975c447e15aeb
hectic4491 <finalrob@gmail.com>
2025-01-16 17:21:25 -0500

    Finished chapter 12

--- 12_Using_this_Keyword.js
+++ 12_Using_this_Keyword.js
@@ -158,4 +158,151 @@ anObject.arrowMethod(); // Logs the global object
 /**Arrow functions are particularly useful for situations like event
  * handlers or when working with methods inside classes, as they help
  * avoid the common pitfalls associated with 'this'.
- */        
+ */
+
+
+/**Function Borrowing
+ * Function borrowing allows us to use the methods of one object on a
+ * different object without having to make a copy of that method and
+ * maintain it in two seperate places. It is accomplished through the use
+ * of .call(), .apply(), or .bind(), all of which exist to explicitly 
+ * set this on the method we are borrowing.
+ * 
+ * 
+ * When we create JavaScript objects, we typically associate them with
+ * certain behavior. Consider the below example:
+ */
+class Dog {
+  constructor(name, age, breed) {
+    this.name = name
+    this.age = age
+    this.breed = breed
+  }
+
+  tellUsAboutYourSelf() {
+    return `My name is ${this.name}. I am a ${this.breed} and I am ${this.age} years old.`
+  }
+
+  woof() {
+    return "WOOF!!!"
+  }
+}
+
+let fido = new Dog("Fido", 3, "dachshund");
+console.log(fido.tellUsAboutYourSelf()); // "My name is Fido..."
+
+/**
+ * We've created a class Dog with three properties and two methods, one
+ * of which is 'tellUsAboutYourSelf()'. We've also created a new instance
+ * of Dog, which is saved into the variable 'fido'. Pretty
+ * straightforward. Now, let's create another class and instantiate a new
+ * instance:
+ */
+
+class Cat {
+  constructor(name, age, breed) {
+    this.name = name
+    this.age = age
+    this.breed = breed
+  }
+
+  meow() {
+    return "MEOW!!!"
+  }
+}
+
+let sparkles = new Cat("Sparkles", 5, "siamese");
+// sparkles.tellUsAboutYourSelf(); // -> throws a TypeError.
+
+/**
+ * We've created a Cat object with the same properties as out Dog object,
+ * but our instance of Cat, which is saved into the variable 'sparkles',
+ * isn't able to tell us about itself. We could refactor our code so that
+ * the class Cat inherits from the class Dog, but in that scenario, all 
+ * of our Cat objects would be able to "woof", which doesn't seem
+ * appropriate or necessary. This is where we use Function Borrowing.
+ * 
+ * 
+ * How does it work?
+ * 
+ * Function borrowing allows us to use the methods of one object on a
+ * different object without having to make a copy of that method and
+ * maintain it in two seperate places. It is accomplished through
+ * the use of .call(), .apply(), or .bind(), all of which exist to
+ * explicitly set 'this' on the method we are borrowing.
+ * 
+ * Given the objects we created above, take a look at function borrowing
+ * in action:
+ */
+
+console.log(fido.tellUsAboutYourSelf.call(sparkles));
+// "My name is sparkles..."
+
+console.log(fido.tellUsAboutYourSelf.apply(sparkles));
+// "My name is sparkles..."
+
+const describeSparkles = fido.tellUsAboutYourSelf.bind(sparkles);
+console.log(describeSparkles());
+// "My name is sparkles..."
+
+/**Each of these examples work because 'this', when referenced inside a
+ * method, refers to the object that received the method call. .call(),
+ * .apply(), and .bind() work by allowing us to alter to object which
+ * 'this' refers inside of the .tellUsAboutYourSelf() method.
+ * 
+ * Whereas .call() and .apply() immediately execute the function call,
+ * .bind() saves the function for later. Once we've saved the borrowed
+ * function into the variable 'describeSparkles', we can call invoke
+ * 'describeSparkles' one hundered lines later and still see the same
+ * output.
+ */
+
+/**Difference between .call() and .apply()?
+ * Both .call() and .apply() are methods used to invoke a function with
+ * a specific 'this' context and arguements, but they differ in how you
+ * pass those arguements.
+ */
+
+/**.call()
+ * Syntax: func.call(thisArg, arg1, arg2, ...)
+ * Usage: You pass arguements, individually.
+ */
+
+/**.apply()
+ * Syntax: func.apply(thisArg, [argsArray])
+ * Usage: You pass as array of arguements.
+ */
+
+// Here's an example to illustrate the difference:
+
+function greet(greeting, punctuation) {
+  return greeting + ' ' + this.name + punctuation;
+}
+
+const person = {
+  name: "Robert"
+};
+
+// Using .call()
+console.log(greet.call(person, 'Hello', '!')); // "Hello Robert!"
+
+// Using .apply()
+console.log(greet.apply(person, ['Hi', '.'])); // "Hi Robert."
+
+
+/**Explicit Binding
+ * 
+ * Explicit binding is when you use the .call(), .apply() or .bind()
+ * methods to explicitly set the value of 'this' in a function.
+ */
+
+
+/**.bind()
+ * The .bind() method in JavaScript allows you to create a new function
+ * with a specific context and optionally preset arguments. Unlike call()
+ * or apply(), bind() does not immediately invoke the function. Instead,
+ * it returns a new function that can be called later, either as a 
+ * regular function or with additional arguements. This is particularly
+ * useful when you want to ensure that a function retains a specific
+ * context, regardless of how or when it's invoked.
+ */
