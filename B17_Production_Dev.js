cbbf4685e6ede5913a3501134d47a7dbfb8a2371
hectic4491 <finalrob@gmail.com>
2025-02-05 10:04:54 -0500

    Working on chapter B17

---
+++ B17_Production_Dev.js
@@ -0,0 +1,19 @@
+// How to think about JavaScript between development and production.
+
+/**Production
+ * 
+ * -> When deploying an app to production, you would want a single,
+ *    bundled and minified.js file. This in theorth helps increase
+ *    performance.
+ */
+
+
+/**Development Environment
+ * 
+ * -> During development, it makes sense to have multiple javascript
+ *    files for an app, because it helps maintain clarity and
+ *    organization.
+ * 
+ * -> When the app is finished, we just have to bundle the code to
+ *    using a bundler to make it more performant and standard.
+ */
