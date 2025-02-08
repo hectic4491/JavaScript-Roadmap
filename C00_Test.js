351e250c2bd779aa3c40b37ad9c0b5c4c2932c00
hectic4491 <finalrob@gmail.com>
2025-02-08 12:32:30 -0500

    Chapter B18

---
+++ C00_Test.js
@@ -0,0 +1,14 @@
+const myObject = {
+  name: 'Alice',
+  age: 20,
+  thing: {cats: 30, dogs: 13},
+  place: "Japan"
+};
+
+
+function myFunction() {
+  let n = myObject.age;
+  console.log(n);
+}
+
+myFunction();
